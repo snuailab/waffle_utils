177c35ab23cc5739c22b9312355d857a4f9d7bd6
ZeroAct <lhj5682@naver.com>
2023-02-16 14:03:11 +0900

    add file io util

---
+++ waffle_utils/file/io.py
@@ -0,0 +1,153 @@
+import json
+import os
+import shutil
+from pathlib import Path
+from typing import Any, Union
+
+import yaml
+
+
+def save_json(obj: Any, fp: Union[str, Path], create_directory: bool = False):
+    """save json file
+
+    Args:
+        obj (Any): Any object that can be converted to json format.
+        fp (Union[str, Path]): file path.
+        create_directory (bool, optional): this determines whether create parent directory or not. Default to False.
+    """
+
+    fp = Path(fp)
+    if create_directory:
+        fp.parent.mkdir(mode=666, parents=True, exist_ok=True)
+
+    with open(fp, "w") as f:
+        json.dump(obj, f, ensure_ascii=False, indent=4)
+
+
+def load_json(fp: Union[str, Path]) -> dict:
+    """load json file
+
+    Args:
+        fp (Union[str, Path]): file path.
+
+    Returns:
+        dict: dictionary
+    """
+
+    fp = Path(fp)
+
+    if not fp.exists():
+        raise FileNotFoundError(f"{fp} does not exists")
+
+    with open(fp) as f:
+        d = json.load(f)
+
+    return d
+
+
+def save_yaml(obj: Any, fp: Union[str, Path], create_directory: bool = False):
+    """save yaml file
+
+    Args:
+        obj (Any): Any object that can be converted to yaml format.
+        fp (Union[str, Path]): file path.
+        create_directory (bool, optional): this determines whether create parent directory or not. Default to False.
+    """
+
+    fp = Path(fp)
+    if create_directory:
+        fp.parent.mkdir(mode=666, parents=True, exist_ok=True)
+
+    with open(fp, "w") as f:
+        yaml.safe_dump(obj, f, indent=4, sort_keys=False)
+
+
+def load_yaml(fp: Union[str, Path]) -> dict:
+    """load yaml file
+
+    Args:
+        fp (Union[str, Path]): file path.
+
+    Returns:
+        dict: dictionary
+    """
+
+    fp = Path(fp)
+
+    if not fp.exists():
+        raise FileNotFoundError(f"{fp} does not exists")
+
+    with open(fp) as f:
+        d = yaml.safe_load(f)
+
+    return d
+
+
+def copy_files_to_directory(
+    src: Union[list, str, Path],
+    dst: Union[str, Path],
+    create_directory: bool = False,
+):
+    """Copy files to directory
+
+    Args:
+        src (Union[list, str, Path]): 'file list' or 'file' or 'directory'.
+        dst (Union[str, Path]): destination directory.
+        create_directory (bool, optional): create destination directory or not. Defaults to False.
+
+    Raises:
+        FileNotFoundError: if src is unknown
+        ValueError: if dst is not directory format
+        FileNotFoundError: if dst is not exists. you can bypass this error with create_directory argument.
+    """
+
+    if isinstance(src, list):
+        src_prefix = os.path.commonpath(src)
+        src_list = list(map(Path, src))
+    elif src.is_file():
+        src = Path(src)
+        src_list = [src]
+        src_prefix = src.parent
+    elif src.is_dir():
+        src = Path(src)
+        src_list = src.glob("**/*")
+        src_prefix = src
+    else:
+        raise FileNotFoundError(f"unknown source {src}")
+
+    dst = Path(dst)
+    if dst.suffix != "":
+        raise ValueError(
+            f"dst should be directory format. but got {dst.suffix}"
+        )
+    elif create_directory:
+        dst.mkdir(mode=0o766, parents=True, exist_ok=True)
+
+    if not dst.exists():
+        raise FileNotFoundError(
+            f"{dst} directory does not exist. please set 'create_directory' argument to be True to make directory."
+        )
+
+    for src_file in src_list:
+        dst_file = Path(str(src_file).replace(str(src_prefix), str(dst)))
+        dst_file.parent.mkdir(mode=0o766, parents=True, exist_ok=True)
+        if src_file.is_file():
+            shutil.copy(src_file, dst_file)
+
+
+def remove_file(src: str):
+    """Remove File
+
+    Args:
+        src (str): file to remove
+    """
+    os.remove(src)
+
+
+def remove_directory(src: str):
+    """Remove Directory
+
+    Args:
+        src (str): file to remove
+    """
+    shutil.rmtree(str(src))
