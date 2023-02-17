4d9c456c6abebb35791153709f4b5863b19d8ffe
ZeroAct <lhj5682@naver.com>
2023-02-17 10:22:13 +0900

    add train_val_split

--- tests/dataset/test_dataset.py
+++ tests/dataset/test_dataset.py
@@ -17,14 +17,18 @@ def test_network():
     network.get_file_from_url(url, dummy_zip_file, create_directory=True)
     io.unzip(dummy_zip_file, dummy_extract_dir, create_directory=True)
 
-    ds = Dataset.from_coco(
-        dummy_dataset_name,
-        dummy_coco_file,
-        dummy_coco_root_dir,
-        root_dir=dummy_data_root_dir,
-    )
+    # ds = Dataset.from_coco(
+    #     dummy_dataset_name,
+    #     dummy_coco_file,
+    #     dummy_coco_root_dir,
+    #     root_dir=dummy_data_root_dir,
+    # )
+
+    ds = Dataset.from_directory(dummy_dataset_name, dummy_data_root_dir)
+
+    ds.split_train_val(train_split_ratio=0.8)
 
     # todo export
     # ds.export
 
-    io.remove_directory(dummy_tmp_dir)
+    # io.remove_directory(dummy_tmp_dir)
