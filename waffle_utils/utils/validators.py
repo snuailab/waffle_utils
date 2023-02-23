01b38c625a8605431434d24f3e90062718359fbc
ZeroAct <huijae.lee@snuailab.ai>
2023-02-23 15:16:16 +0900

    add strict flag to validator

--- waffle_utils/utils/validators.py
+++ waffle_utils/utils/validators.py
@@ -1,8 +1,10 @@
-def type_validator(_type: type):
+def type_validator(_type: type, strict: bool = True):
     def type_check(f):
         def input_check(self, v, *args, **kwargs):
             if v is not None:
-                if not isinstance(v, _type):
+                if not strict:
+                    v = _type(v)
+                elif not isinstance(v, _type):
                     raise TypeError(f"value {v} should be {_type}")
             return f(self, v, *args, **kwargs)
 
