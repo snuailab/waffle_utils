6bf151efa67267a6dd4f15e905439a258d8b5a94
ZeroAct <huijae.lee@snuailab.ai>
2023-02-23 15:07:09 +0900

    add value in error msg

--- waffle_utils/utils/validators.py
+++ waffle_utils/utils/validators.py
@@ -3,7 +3,7 @@ def type_validator(_type: type):
         def input_check(self, v, *args, **kwargs):
             if v is not None:
                 if not isinstance(v, _type):
-                    raise TypeError(f"value should be {_type}")
+                    raise TypeError(f"value {v} should be {_type}")
             return f(self, v, *args, **kwargs)
 
         return input_check
