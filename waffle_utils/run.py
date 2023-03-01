f02b6893d94a33bb532b38a165810443e0d3d38e
ByteBrewer <heesu-kim@snuailab.ai>
2023-03-01 19:45:09 +0900

    add the CLIs, `extract_frames` and `create_video`

--- waffle_utils/run.py
+++ waffle_utils/run.py
@@ -25,6 +25,15 @@ train_split_ratio_docs = (
 # export docs
 format_docs = f"[{', '.join(map(lambda x: x.name, Format))}]"
 
+# video processing docs
+input_video_path_docs = "input video file path"
+input_images_path_docs = "input image files path"
+output_images_path_docs = "output images path"
+output_video_path_docs = "output video file path"
+frame_rate_docs = "frame rate"
+fps_docs = "frames per second"
+verbose_docs = "verbose"
+
 
 @app.command(name="get_file_from_url")
 def _get_file_from_url(
@@ -94,15 +103,30 @@ def _export(
     ds.export(Format[export_format])
 
 
-@app.command(name="video_to_frames")
-def _video_to_frames(
-    # TODO: Add `help`s
-    input_video_path: str = typer.Option(...),
-    output_dir: str = typer.Option(...),
-    frame_rate: int = typer.Option(...),
+@app.command(name="extract_frames")
+def _extract_frames(
+    input_path: str = typer.Option(..., help=input_video_path_docs),
+    output_path: str = typer.Option(..., help=output_images_path_docs),
+    frame_rate: int = typer.Option(30, help=frame_rate_docs),
+    verbose: bool = typer.Option(False, help=verbose_docs),
 ):
-    video_converter = VideoTools.video_to_frames(input_video_path, output_dir)
-    video_converter.extract_frames(frame_rate)
+    """Extract Frames from a Video File"""
+
+    vt = VideoTools.from_video(input_path, output_path)
+    vt.extract_frames(frame_rate, verbose)
+
+
+@app.command(name="create_video")
+def _create_video(
+    input_path: str = typer.Option(..., help=input_images_path_docs),
+    output_path: str = typer.Option(..., help=output_video_path_docs),
+    fps: int = typer.Option(30, help=fps_docs),
+    verbose: bool = typer.Option(False, help=verbose_docs),
+):
+    """Create a Video from Frame Image Files"""
+
+    vt = VideoTools.from_images(input_path, output_path)
+    vt.create_video(fps, verbose)
 
 
 if __name__ == "__main__":
