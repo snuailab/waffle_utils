121903aa23378e79217cffd7b680880992a69922
Heesu <heesu-kim@snuailab.ai>
2023-03-11 20:01:15 +0900

    Feature/video frame extractor (#35)
    * Add support for additional video formats
    
    * Rename config script
    
    * Update docstring
    
    * Add supported image extension
    
    * Test all image and video extensions
    
    * Enhance verbose for video tools
    
    * Add supported extensions for video and image
    
    * Modulize getting extension
    
    * debug file serach
    
    * Refactor video creator

--- waffle_utils/video/config.py
+++ waffle_utils/video/config.py
@@ -1,3 +1,5 @@
+import cv2
+
 # Image configurations
 DEFAULT_IMAGE_EXTENSION = "jpg"
 SUPPORTED_IMAGE_EXTENSION = ["jpg", "jpeg", "png", "bmp", "tif", "tiff"]
@@ -5,3 +7,15 @@ SUPPORTED_IMAGE_EXTENSION = ["jpg", "jpeg", "png", "bmp", "tif", "tiff"]
 # Video configurations
 DEFAULT_FRAME_RATE = 30
 SUPPORTED_VIDEO_EXTENSION = ["mp4", "avi", "wmv", "mov", "flv", "mkv", "mpeg"]
+FOURCC_MAP = {
+    "mp4": cv2.VideoWriter_fourcc(*"mp4v"),
+    "avi": cv2.VideoWriter_fourcc(*"xvid")
+    if cv2.VideoWriter_fourcc(*"xvid") == -1
+    else cv2.VideoWriter_fourcc(*"mjpg"),
+    "wmv": cv2.VideoWriter_fourcc(*"wmv2"),
+    "mov": cv2.VideoWriter_fourcc(*"xvid"),
+    "flv": cv2.VideoWriter_fourcc(*"flv1"),
+    "mkv": cv2.VideoWriter_fourcc(*"vp80"),
+    "mpeg": cv2.VideoWriter_fourcc(*"xvid"),
+    "mpg": cv2.VideoWriter_fourcc(*"xvid"),
+}
