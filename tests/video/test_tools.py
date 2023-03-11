4426604bb830f56e442b32840e86e1e9fee494fc
Heesu <heesu-kim@snuailab.ai>
2023-03-11 13:16:44 +0900

    Feature/video frame extractor (#34)
    * Add support for additional video formats
    
    * Rename config script
    
    * Update docstring
    
    * Add supported image extension
    
    * Test all image and video extensions
    
    * Enhance verbose for video tools
    
    * Add supported extensions for video and image

--- tests/video/test_tools.py
+++ tests/video/test_tools.py
@@ -2,7 +2,10 @@ import tempfile
 from pathlib import Path
 
 from waffle_utils.file.io import copy_file, remove_directory
-from waffle_utils.video.config import SUPPORTED_VIDEO_EXT
+from waffle_utils.video.config import (
+    SUPPORTED_IMAGE_EXTENSION,
+    SUPPORTED_VIDEO_EXTENSION,
+)
 from waffle_utils.video.tools import create_video, extract_frames
 
 
@@ -12,27 +15,35 @@ def test_tools():
         temp_dir = Path(temp_dir)
         input_path = Path("tests/video/test.mp4")
         input_path_copy = temp_dir / "test.mp4"
-        output_dir = temp_dir / "frames"
 
         # Copy the input file to the temporary directory
         copy_file(input_path, input_path_copy)
 
-        # Extract and save individual image frames from video
-        extract_frames(input_path, output_dir, frame_rate=30, verbose=True)
+        # Iterate over all combinations of image and video format
+        for image_extension in SUPPORTED_IMAGE_EXTENSION:
+            for video_extension in SUPPORTED_VIDEO_EXTENSION:
+                # Create a subdirectory for this combination
+                subdir_name = f"{image_extension}_to_{video_extension}"
+                output_dir = temp_dir / subdir_name
+                output_dir.mkdir()
 
-        # Check that the frames were extracted correctly
-        assert len(list(output_dir.glob("*.jpg"))) == 209
-        assert all(frame.is_file() for frame in output_dir.glob("*.jpg"))
+                # Extract and save individual image frames from video
+                extract_frames(
+                    input_path_copy,
+                    output_dir,
+                    frame_rate=30,
+                    output_image_extension=image_extension,
+                    verbose=True,
+                )
 
-        # Create a video file from the extracted frames with different extensions
-        input_dir = output_dir
-        for ext in SUPPORTED_VIDEO_EXT:
-            output_path = temp_dir / f"test_create_video{ext}"
-            create_video(input_dir, output_path, frame_rate=10, verbose=True)
+                # Create a video file from the extracted frames
+                input_dir = output_dir  # Use the images directory as input
+                output_path = temp_dir / f"test_{subdir_name}.{video_extension}"
+                create_video(input_dir, output_path, frame_rate=10, verbose=True)
 
-            # Check that the video file was created correctly
-            assert output_path.is_file()
-            assert output_path.stat().st_size > 0
+                # Check that the video file was created correctly
+                assert output_path.is_file()
+                assert output_path.stat().st_size > 0
 
         # Clean up
         remove_directory(temp_dir)
--- waffle_utils/video/config.py
+++ waffle_utils/video/config.py
@@ -1,10 +1,7 @@
+# Image configurations
+DEFAULT_IMAGE_EXTENSION = "jpg"
+SUPPORTED_IMAGE_EXTENSION = ["jpg", "jpeg", "png", "bmp", "tif", "tiff"]
+
+# Video configurations
 DEFAULT_FRAME_RATE = 30
-SUPPORTED_VIDEO_EXT = [
-    ".mp4",
-    ".avi",
-    ".wmv",
-    ".mov",
-    ".flv",
-    ".mkv",
-    ".mpeg",
-]
+SUPPORTED_VIDEO_EXTENSION = ["mp4", "avi", "wmv", "mov", "flv", "mkv", "mpeg"]
