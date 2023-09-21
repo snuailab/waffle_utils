7e454fa4584a7ba7c8a20ebb70dea9e4d3948927
Jiyong-Jeon <jiyong@snuailab.ai>
2023-09-21 11:29:21 +0900

    Hotfix: save_image

--- waffle_utils/image/io.py
+++ waffle_utils/image/io.py
@@ -3,11 +3,17 @@ from typing import Union
 
 import cv2
 import numpy as np
+from file.io import make_directory
 
 Mat = np.ndarray[int, np.dtype[np.generic]]
 
 
-def save_image(output_path: Union[str, Path], image: Mat) -> None:
+def save_image(
+    output_path: Union[str, Path], image: Mat, create_directory: bool = False
+) -> None:
+    output_path = Path(output_path)
+    if create_directory:
+        make_directory(output_path.parent)
 
     cv2.imwrite(str(output_path), image)
 
